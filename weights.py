6d3ff8639752d580d8e99396b7eac0b0c1e0e911
giulio mandorli <giulio.mandorli@cern.ch>
2019-06-07 16:59:24 +0200

    btagWeight added

--- weights.py
+++ weights.py
@@ -8,7 +8,7 @@ def addDefaultWeights(flow):
 #   flow.Define("SelectedJet_btagWeight","vector_map(btagWeight,SelectedJet_btagCSVV2,SelectedJet_pt,SelectedJet_eta)")
 #   flow.Define("btagEventWeight","isMC?(std::accumulate(SelectedJet_btagWeight.begin(),SelectedJet_btagWeight.end(),1, std::multiplies<double>())):1.f")
     flow.CentralWeight("genWeight")
-#    flow.CentralWeight("btagWeight_CSVV2")
+    flow.CentralWeight("btagWeight")
     flow.CentralWeight("puWeight")
 
 def addMuEffWeight(flow):
