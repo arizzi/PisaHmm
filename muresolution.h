cbc59cfa83bfbbc28a5c77f0efb49814aafbbfb3
leonardo <leonardo.giannini@cern.ch>
2019-06-10 16:34:51 +0200

    muon resolution methods and file

---
+++ muresolution.h
@@ -0,0 +1,21 @@
+#ifndef MURESOLUTION_H
+#define MURESOLUTION_H
+
+#include <TH2F.h>
+#include <TFile.h>
+#include <math.h> 
+
+TFile* f_param= TFile::Open("muonresolution.root");
+TH2F *hmuon= (TH2F*)f_param->Get("PtErrParametrization");
+
+float hRelResolution(float pt1, float eta1, float pt2, float eta2) {
+    float MuonPtErr1=hmuon->GetBinContent(hmuon->FindBin( (log(pt1)),abs(eta1) ));
+    float MuonPtErr2=hmuon->GetBinContent(hmuon->FindBin( (log(pt2)),abs(eta2) ));    
+    return sqrt(0.5*(pow(MuonPtErr1,2)+pow(MuonPtErr2,2)));
+}  
+
+float muonRelResolution(float pt, float eta) {
+    return hmuon->GetBinContent(hmuon->FindBin( (log(pt)),abs(eta) ));
+}
+
+#endif
