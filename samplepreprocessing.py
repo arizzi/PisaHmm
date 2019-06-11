c7c21a6bf8f123f768e1f2803ea5f4e23aa0d661
arizzi <andrea.rizzi@cern.ch>
2019-06-11 08:54:16 +0200

    Update samplepreprocessing.py

--- samplepreprocessing.py
+++ samplepreprocessing.py
@@ -3,7 +3,8 @@ import ROOT
 import sys
 
 #flow=SampleProcessing("VBF Hmumu Analysis","/scratch/arizzi/Hmm/nail/samples/6B8A2AC8-35E6-1146-B8A8-B1BA90E3F3AA.root")
-flow=SampleProcessing("VBF Hmumu Analysis","/scratch/mandorli/Hmumu/fileSkimFromNanoAOD/fileSkim2016_Z/VBF_HToMuMu_nano2016.root")
+#flow=SampleProcessing("VBF Hmumu Analysis","/scratch/mandorli/Hmumu/fileSkimFromNanoAOD/fileSkim2016_Z/VBF_HToMuMu_nano2016.root")
+flow=SampleProcessing("VBF Hmumu Analysis","/eos/user/a/arizzi/Hmm/data/VBF_HToMuMu_nano2018.root")
 
 flow.SubCollection("GenLepton","GenPart",sel="(abs(GenPart_pdgId)==13 || abs(GenPart_pdgId)==11 || abs(GenPart_pdgId)==15)")
 flow.MatchDeltaR("GenLepton","GenJet") 
