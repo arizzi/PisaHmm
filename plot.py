932eb119dc252761b4ab1f86f40719ef932f5645
giulio mandorli <giulio.mandorli@cern.ch>
2019-06-14 12:19:33 +0200

    Workspace takes year as input

--- plot.py
+++ plot.py
@@ -273,7 +273,7 @@ def makeplot(hn,saveintegrals=True):
      fill_datasum (f, gr, model.signal, SumTH1=histoSigsum, stack=histosSig, stackSys=histoSigsumSyst, hn=hn, myLegend=myLegend, ftxt=ftxt, lumi=lumitot) 
    
    if makeWorkspace : 
-       WorkSpace.WorkSpace(model, all_histo_all_syst)
+       WorkSpace.WorkSpace(model, all_histo_all_syst, year)
        return 
    
    histosum[hn].Add(histoSigsum[hn])
