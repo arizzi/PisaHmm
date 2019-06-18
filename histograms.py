81b395a207b63af2486cd32ccba4c3358a3325e6
leonardo <leonardo.giannini@cern.ch>
2019-06-18 17:48:14 +0200

    more histograms

--- histograms.py
+++ histograms.py
@@ -1,8 +1,10 @@
 #define hist mapping
 genericHistos=["Higgs_m","LeadMuon_pt","LeadMuon_eta","SubMuon_pt","SubMuon_eta","QJet0_eta","QJet1_eta","QJet0_pt_touse","QJet1_pt_touse","PV_npvs","LeadingSAJet_pt","SAHT","SAHT5","nFootprintSAJet","FootHT"]
-bdtInputHistos=["Mqq_log","mmjj_pt","qqDeltaEta","NSoft5","ll_zstar","Higgs_pt","theta2","mmjj_pz","MaxJetAbsEta","Higgs_m_uncalib","NSoft5New","ll_zstar_log"]
+bdtInputHistos=["Mqq_log","mmjj_pt","qqDeltaEta","NSoft5","ll_zstar","Higgs_pt","theta2","mmjj_pz","MaxJetAbsEta","Higgs_m_uncalib","NSoft5New","ll_zstar_log", "Higgs_mReso"]
 signalHistosVariations=["SBClassifierNoMass","BDTAtanNoMass"]
-signalHistos=["SBClassifier","BDTAtan","LHE_NpNLO"]
+signalHistos=["SBClassifier","BDTAtan","LHE_NpNLO","DNNClassifier", "DNNAtan"]
+
+#signalHistos=["SBClassifier"]
 
 histosPerSelection={
 #"PreSel" : genericHistos+["Mqq"],
