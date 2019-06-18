d101d04f260acca28ac5b0dbd28d15ac2db2134a
giulio mandorli <giulio.mandorli@cern.ch>
2019-06-18 16:36:39 +0200

    systematics has been written in a function in systematicGrouping

--- models2016H.py
+++ models2016H.py
@@ -55,45 +55,7 @@ markercolor=fillcolor
 
 from rebinning import *
 
-
-legendGrouping = {}
-legendGrouping.update(background)
-legendGrouping.update(signal)
-
-
-#TODO: separate systematics as
-#shape only (remove normalization effects)
-#per group 
-#per sample
-
-#default is correlated among all samples 
-#and correlated nuisance for norm+shape
-systematicDetail={
-"QCDScale" : {
-        "type": "ShapeOn", #NormOnly, ShapeNorm
-        "decorrelate":legendGrouping
-        },
-"puWeight" : {
-          "type": "ShapeOn" #NormOnly, ShapeNorm
-        },
-"lumi":{
-        "type": "lnN",
-        "value":1.025
-    },
-"VVxsec":{
-        "type": "lnN",
-        "decorrelate":{"ZZ":["ZZ2l2q","ZZ2l2n","ZZ4l"],"WZ":["WZ1l3n","WZ2l2q","WZ3l1n", "WZ1l1n2q"],"WW":["WWdps","WWJJlnln","WLLJJln", "WW2l2n","WWlnqq"]},
-        "value":1.10,
-        "groupvalue":{"ZZ":2.,"WZ":3.,"WW":4.},
-        #"samplevalue":{}
-   },
-"WJetsxsec":{
-        "type": "lnN",
-        "decorrelate":{"WJets":["W2J","W1J","W0J"]},
-        "value":1.10,
-        #"groupvalue":{},
-        "samplevalue":{"W2J":2.,"W1J":3.,"W0J":4.}
-   }
-}
+from systematicGrouping import *
+systematicDetail = systematicGrouping(background, signal)
 
 
