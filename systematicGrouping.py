275ccae2eb385cd03bef573efdbce0a6d957e41f
giulio mandorli <giulio.mandorli@cern.ch>
2019-06-25 19:31:49 +0200

    LHE scale added in workspace

--- systematicGrouping.py
+++ systematicGrouping.py
@@ -17,12 +17,18 @@ def systematicGrouping (background, signal) :
     legendGrouping.update(background)
     legendGrouping.update(signal)
 
+    DY = ["DY105","DY105VBF", "DY0J", "DY1J", "DY2J"]
+    EWK= ["EWKZ", "EWKZint"]
+    TT = ["TTlep","TTsemi","TThad", "TT"]
+    ST = ["STs","STwtbar","STwt","STtbar","STt"]
+    WW = ["WWdps","WWJJlnln","WLLJJln", "WW2l2n","WWlnqq"]
+    WZ = ["WZ1l3n","WZ2l2q","WZ3l1n", "WZ1l1n2q"]
+    ZZ = ["ZZ2l2q","ZZ2l2n","ZZ4l"]
+    WJets = ["W2J","W1J","W0J"]
+    Hmm = ["vbfHmm","ggHmm", "zHmm", "WplusHmm", "WminusHmm", "ttHmm"]
 
+    
     systematicDetail={
-        #"QCDScale" : {
-                #"type": "shape", #NormOnly, ShapeNorm
-                #"decorrelate":legendGrouping
-                #},
         "puWeight" : {
                 "type": "shape" #NormOnly, ShapeNorm
                 },
@@ -32,31 +38,31 @@ def systematicGrouping (background, signal) :
             },
         "VVxsec":{
                 "type": "lnN",
-                "decorrelate":{"ZZ":["ZZ2l2q","ZZ2l2n","ZZ4l"],"WZ":["WZ1l3n","WZ2l2q","WZ3l1n", "WZ1l1n2q"],"WW":["WWdps","WWJJlnln","WLLJJln", "WW2l2n","WWlnqq"]},
+                "decorrelate":{"ZZ":ZZ,"WZ":WZ,"WW":WW},
                 "value":1.10,
                 #"groupvalue":{"ZZ":2.,"WZ":3.,"WW":4.},
                 #"samplevalue":{}
         },
         "WJetsxsec":{
                 "type": "lnN",
-                "decorrelate":{"WJets":["W2J","W1J","W0J"]},
+                "decorrelate":{"WJets":WJets},
                 "value":1.10,
                 #"groupvalue":{},
                 #"samplevalue":{"W2J":2.,"W1J":3.,"W0J":4.}
         },
         "DYxsec":{
                 "type": "lnN",
-                "decorrelate":{"DY":["DY105","DY105VBF"]},
+                "decorrelate":{"DY":DY},
                 "value":1.10,
         },
         "TTxsec":{
                 "type": "lnN",
-                "decorrelate":{"DY":["TTlep","TTsemi"]},
+                "decorrelate":{"TT":TT},
                 "value":1.05,
         },
         "STxsec":{
                 "type": "lnN",
-                "decorrelate":{"DY":["STs","STwtbar","STwt","STtbar","STt"]},
+                "decorrelate":{"ST":ST},
                 "value":1.05,
         },
         "JER":{
@@ -71,6 +77,16 @@ def systematicGrouping (background, signal) :
                 #"type": "shape",
                 #"value":1.0,
         #}
+        "LHERen":{
+                "type": "shape",
+                "decorrelate":{"Hmm":Hmm, "DY":DY, "EWK":EWK, "TT":TT ,"ST":ST, "WJets":WJets, "ZZ":ZZ, "WZ":WZ, "WW":WW},
+                "value":1.0,
+        },
+        "LHEFac":{
+                "type": "shape",
+                "decorrelate":{"Hmm":Hmm, "DY":DY, "EWK":EWK, "TT":TT ,"ST":ST, "WJets":WJets, "ZZ":ZZ, "WZ":WZ, "WW":WW},
+                "value":1.0,
+        }
     }
     
     
