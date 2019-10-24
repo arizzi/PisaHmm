212e959914529ad58c9b390ca70da77b943fa44a
silviodonato <silvio.donato@cern.ch>
2019-10-24 19:24:19 +0200

    reduce log output in plot.py

--- plot.py
+++ plot.py
@@ -310,7 +310,7 @@ def makeEnvelopeShape(hn,sy,f, d, model):
     sums = [0]*len(ratio)
     sumSquares = [0]*len(ratio)
     i = 0 ## nominal is the first entry.
-    hs=f[d].Get(findSyst(hn,pdf+str(i),f[d]))
+    hs=f[d].Get(findSyst(hn,pdf+str(i),f[d], silent=True))
     hs = hs.Rebin(len(envelopeBinning)-1, hs.GetName(), array('d',envelopeBinning))
 #        Calculate ratio wrt to PDF0:
     hs0=hs
@@ -337,7 +337,7 @@ def makeEnvelopeShape(hn,sy,f, d, model):
         ratio.SetBinError(bin_, rms)
 
     meanrms/=ngood if ngood !=0 else 1 
-    print 'mean rms',meanrms
+#    print 'mean rms',meanrms
     #        print bin_, i, sum_, sumSquare, rms
         
     
@@ -356,7 +356,7 @@ def makeEnvelopeShape(hn,sy,f, d, model):
 
     #ratio.Fit(funct,"QN0")
     funct = ROOT.TF1("funct",envelopeFunction.format(up=(1. if "Up" in sy else -1.),rms=meanrms,xmin=nomHistoRebinned.GetXaxis().GetXmin(),xmax=nomHistoRebinned.GetXaxis().GetXmax()),nomHistoRebinned.GetXaxis().GetXmin(),nomHistoRebinned.GetXaxis().GetXmax())
-    print "funct",envelopeFunction.format(up=(1. if "Up" in sy else -1.),rms=meanrms,xmin=nomHistoRebinned.GetXaxis().GetXmin(),xmax=nomHistoRebinned.GetXaxis().GetXmax())
+#    print "funct",envelopeFunction.format(up=(1. if "Up" in sy else -1.),rms=meanrms,xmin=nomHistoRebinned.GetXaxis().GetXmin(),xmax=nomHistoRebinned.GetXaxis().GetXmax())
     nhisto = f[d].Get(hn).Clone(hn+sy)
     if hn.split("___")[0] in model.rebin.keys(): nhisto = (nhisto.Rebin(len(model.rebin[hn.split("___")[0]])-1,"hnew"+sy,array('d',model.rebin[hn.split("___")[0]]))).Clone(hn+"rebinned")
     nhisto.Multiply(funct)
@@ -471,8 +471,13 @@ def fill_datasum(f, gr, samplesToPlot, SumTH1, stack, stackSys, hn, myLegend, ft
                 for sy in systematicsSetToUse :
                     sy_base = sy.replace("Up", "").replace("Down", "")
                     if not data :
-                        if sy_base in model.systematicDetail and "alternativeSample" in model.systematicDetail[sy_base] and d in model.systematicDetail[sy_base]["alternativeSample"]:
-                            hs=makeAlternativeShape(hn,sy,f, d, model.systematicDetail[sy_base]["alternativeSample"][d], model.systematicDetail[sy_base]["powerDown"] if model.systematicDetail[sy_base] else -1)
+                        if sy_base in model.systematicDetail and "alternativeSample" in model.systematicDetail[sy_base]:
+                            if d in model.systematicDetail[sy_base]["alternativeSample"]:
+                                hs=makeAlternativeShape(hn,sy,f, d, model.systematicDetail[sy_base]["alternativeSample"][d], model.systematicDetail[sy_base]["powerDown"] if model.systematicDetail[sy_base] else -1)
+                            else: 
+                                hs = f[d].Get(hn).Clone(hn+sy)
+                                if hs and hn.split("___")[0] in model.rebin.keys() : 
+                                    hs = (hs.Rebin(len(model.rebin[hn.split("___")[0]])-1,"hnew"+sy,array('d',model.rebin[hn.split("___")[0]]))).Clone(hs.GetName()+"rebinned")
                         elif sy_base in model.systematicDetail and "envelope" in model.systematicDetail[sy_base]:
                             hs=makeEnvelopeShape(hn,sy,f, d, model)
                         else:
@@ -495,8 +500,13 @@ def fill_datasum(f, gr, samplesToPlot, SumTH1, stack, stackSys, hn, myLegend, ft
                 SumTH1[hn].Add(h)	
                 for sy in systematicsSetToUse :
                     sy_base = sy.replace("Up", "").replace("Down", "")
-                    if sy_base in model.systematicDetail and "alternativeSample" in model.systematicDetail[sy_base] and d in model.systematicDetail[sy_base]["alternativeSample"]:
-                        hs=makeAlternativeShape(hn,sy,f, d, model.systematicDetail[sy_base]["alternativeSample"][d], model.systematicDetail[sy_base]["powerDown"] if model.systematicDetail[sy_base] else -1)
+                    if sy_base in model.systematicDetail and "alternativeSample" in model.systematicDetail[sy_base]:
+                        if d in model.systematicDetail[sy_base]["alternativeSample"]:
+                            hs=makeAlternativeShape(hn,sy,f, d, model.systematicDetail[sy_base]["alternativeSample"][d], model.systematicDetail[sy_base]["powerDown"] if model.systematicDetail[sy_base] else -1)
+                        else:
+                            hs = f[d].Get(hn).Clone(hn+sy)
+                            if hs and hn.split("___")[0] in model.rebin.keys() : 
+                                hs = (hs.Rebin(len(model.rebin[hn.split("___")[0]])-1,"hnew"+sy,array('d',model.rebin[hn.split("___")[0]]))).Clone(hs.GetName()+"rebinned")
                     elif sy_base in model.systematicDetail and "envelope" in model.systematicDetail[sy_base]:
                         hs=makeEnvelopeShape(hn,sy,f, d, model)
                     else:
@@ -512,6 +522,7 @@ def fill_datasum(f, gr, samplesToPlot, SumTH1, stack, stackSys, hn, myLegend, ft
                             else : hs.Scale(samples[d]["xsec"]*lumi)
                         addHistoInTStack (hs, stackSys, all_histo_all_syst, gr, hn, sy, d, makeWorkspace) 
                     else :
+                        print "missing",sy,"for",hn, gr,d 
                         addHistoInTStack (h, stackSys, all_histo_all_syst, gr, hn, sy, d, makeWorkspace) 
             stack[hn].Add(h)
             #if n==0 : stack[hn].Add(h)
