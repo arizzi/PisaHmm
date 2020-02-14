55f85e046dcdd8822dc24ec7a0e4298e9c1c477e
silviodonato <silvio.donato@cern.ch>
2020-02-14 13:25:27 +0100

    recover the first PDF implementation

--- systematicGrouping.py
+++ systematicGrouping.py
@@ -121,6 +121,19 @@ def systematicGrouping (background, signal,jesList) :
       #          "envelopeNBins": 6,
        #         "envelopeFunction": "1.+{up}*{rms}",
        # },
+       ### Old PDF ###
+        #"PDFX1":{
+                #"type": "shapeOnly",
+                #"decorrelate":{
+                    #"ggH":["ggHmm"],
+                    #"vbfHmm":["vbfHmm"],
+                    #"DY" :["DY105VBF","DY105"],
+                    #"EWKZ" :["EWKZ","EWKZ105"],
+                #},
+                #"envelope": "LHEPdf",
+                #"envelopeNBins": 6,
+                #"envelopeFunction": "{up}*2*(x-0.5*({xmax}+{xmin}))/({xmax}-{xmin})",
+        #},
         "PDFX1":{
                 "type": "shapeOnly",
                 "decorrelate":{
@@ -130,8 +143,10 @@ def systematicGrouping (background, signal,jesList) :
                     "EWKZ" :["EWKZ","EWKZ105"],
                 },
                 "envelope": "LHEPdf",
-                "envelopeNBins": 6,
-                "envelopeFunction": "{up}*2*(x-0.5*({xmax}+{xmin}))/({xmax}-{xmin})",
+                "envelopeFunction": "[0] + [1]*x",
+                "envelopeFunctionParameter": 1,
+                "envelopeFunctionParameterValues": (1, 0),
+                "envelopeFunctionRange": (0. , 4.5)
         },
     }
     jes={x[7:-4]:{"type": "shape", "value":1.0} for x in jesList if "Down" in x}
