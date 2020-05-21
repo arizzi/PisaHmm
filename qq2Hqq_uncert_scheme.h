25e3587a08cd7e46422f970c7303401fffd4f9ee
silviodonato <silvio.donato@cern.ch>
2020-05-21 14:55:54 +0200

    add qq2Hqq_uncert_scheme files

---
+++ qq2Hqq_uncert_scheme.h
@@ -0,0 +1,14 @@
+// Authors:
+//    - Claudia Bertella
+//    - Yacine Haddad
+//
+// email:  yhaddad@cern.ch
+
+#include <vector>
+#include <map>
+#include <iostream>
+#include <iomanip>
+#include <numeric>
+#include <cmath>
+
+double vbf_uncert_stage_1_1(int source, int event_STXS, double Nsigma=1.0);
